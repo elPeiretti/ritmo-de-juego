b677435f0f1eef5aa4d1411b1a8e24104743d280
elPeiretti <tomaspeiretti@gmail.com>
2024-09-15 16:41:46 -0300

    feat: create row resource for clubs

---
+++ app/src/main/java/com/elpeiretti/ritmodejuego/fragment/club/ClubRecyclerAdapter.java
@@ -0,0 +1,51 @@
+package com.elpeiretti.ritmodejuego.fragment.club;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.elpeiretti.ritmodejuego.databinding.RowClubBinding;
+import com.elpeiretti.ritmodejuego.domain.Club;
+
+import java.util.List;
+
+public class ClubRecyclerAdapter extends RecyclerView.Adapter<ClubRecyclerAdapter.ViewHolder> {
+
+    private List<Club> clubs;
+
+    public ClubRecyclerAdapter(List<Club> clubs) {
+        this.clubs = clubs;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RowClubBinding rowBinding = RowClubBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new ViewHolder(rowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Club club = clubs.get(position);
+        holder.clubName.setText(club.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return clubs.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView clubName;
+
+        public ViewHolder(RowClubBinding rowBinding) {
+            super(rowBinding.getRoot());
+            clubName = rowBinding.clubName;
+        }
+    }
+
+}
