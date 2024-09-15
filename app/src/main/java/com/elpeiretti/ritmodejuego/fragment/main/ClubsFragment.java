f40f7b47dbffbe77134525ef4d3de4458f49e902
elPeiretti <tomaspeiretti@gmail.com>
2024-09-15 16:20:06 -0300

    feat: create fragments for clubs view

---
+++ app/src/main/java/com/elpeiretti/ritmodejuego/fragment/main/ClubsFragment.java
@@ -0,0 +1,23 @@
+package com.elpeiretti.ritmodejuego.fragment.main;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.elpeiretti.ritmodejuego.databinding.FragmentClubsBinding;
+
+public class ClubsFragment extends Fragment {
+
+    private FragmentClubsBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentClubsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
