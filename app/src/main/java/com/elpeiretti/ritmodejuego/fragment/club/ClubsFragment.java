073f175cd9160778f9a2e5af73cdf7b9b397958e
elPeiretti <tomaspeiretti@gmail.com>
2024-09-15 17:01:53 -0300

    feat: implement ClubRecycler in ClubsFragment

--- app/src/main/java/com/elpeiretti/ritmodejuego/fragment/club/ClubsFragment.java
+++ app/src/main/java/com/elpeiretti/ritmodejuego/fragment/club/ClubsFragment.java
@@ -4,11 +4,19 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.elpeiretti.ritmodejuego.databinding.FragmentClubsBinding;
+import com.elpeiretti.ritmodejuego.domain.Club;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ClubsFragment extends Fragment {
 
@@ -18,6 +26,19 @@ public class ClubsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentClubsBinding.inflate(inflater, container, false);
+
+        RecyclerView recycler = binding.clubsRecycler;
+        recycler.setLayoutManager(new LinearLayoutManager(this.requireContext()));
+        recycler.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
+        // TODO: update with repository
+        ArrayList<Club> clubList = new ArrayList<>();
+        for(int i=1; i<11; i++) {
+            Club c = new Club();
+            c.setId((long) i);
+            c.setName("Club Number " + i);
+            clubList.add(c);
+        }
+        recycler.setAdapter(new ClubRecyclerAdapter(clubList));
         return binding.getRoot();
     }
 }
