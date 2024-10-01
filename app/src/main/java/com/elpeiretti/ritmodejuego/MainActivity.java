5b795a3b99ac2b11c9810dc0c8084e8d387fcd66
elPeiretti <tomaspeiretti@gmail.com>
2024-09-30 21:43:10 -0300

    feat: add menu items to toolbar

--- app/src/main/java/com/elpeiretti/ritmodejuego/MainActivity.java
+++ app/src/main/java/com/elpeiretti/ritmodejuego/MainActivity.java
@@ -1,6 +1,7 @@
 package com.elpeiretti.ritmodejuego;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -12,15 +13,45 @@ import com.elpeiretti.ritmodejuego.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding viewBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
 
         NavController navController = NavHostFragment.findNavController(viewBinding.navHostFragment.getFragment());
         NavigationUI.setupWithNavController(viewBinding.toolbar, navController,
                 new AppBarConfiguration.Builder(navController.getGraph()).build());
 
+        viewBinding.toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.action_ver_clubs) {
+                navController.navigate(R.id.action_ritmoDeJuegoFragment_to_clubsFragment);
+                return true;
+            } else if (item.getItemId() == R.id.action_crear_club) {
+                navController.navigate(R.id.action_clubsFragment_to_editarClubFragment);
+                return true;
+            }
+            return false;
+        });
+
+        navController.addOnDestinationChangedListener((controller, navDestination, bundle) -> {
+            int destinationId = navDestination.getId();
+            if (destinationId == R.id.clubsFragment)
+                setMenuItemVisible(1);
+            else if (destinationId == R.id.editarClubFragment)
+                setMenuItemVisible(2);
+            else
+                setMenuItemVisible(0);
+        });
+
+        setMenuItemVisible(0);
         setContentView(viewBinding.getRoot());
+    }
+
+    private void setMenuItemVisible(int itemIndex) {
+        Menu menu = viewBinding.toolbar.getMenu();
+        for (int i=0 ; i<menu.size(); i++)
+            menu.getItem(i).setVisible(i == itemIndex);
     }
 }
