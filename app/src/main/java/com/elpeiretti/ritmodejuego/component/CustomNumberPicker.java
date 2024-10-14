eb44b2ca550c8b7df631ba1487a58994d6543363
elPeiretti <tomaspeiretti@gmail.com>
2024-10-14 20:28:47 -0300

    refactor: simplify CustomNumberPicker

--- app/src/main/java/com/elpeiretti/ritmodejuego/component/CustomNumberPicker.java
+++ app/src/main/java/com/elpeiretti/ritmodejuego/component/CustomNumberPicker.java
@@ -5,14 +5,11 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
-import android.widget.NumberPicker;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 
 import com.elpeiretti.ritmodejuego.R;
 import com.elpeiretti.ritmodejuego.databinding.CustomNumberPickerBinding;
-import com.elpeiretti.ritmodejuego.util.TextChangedListener;
 
 import java.util.function.Consumer;
 
@@ -20,52 +17,26 @@ public class CustomNumberPicker extends LinearLayout {
 
     private final CustomNumberPickerBinding binding;
 
-    private String dialogTitle = "Select number";
-
-    private final NumberPicker numberPicker;
-
     public CustomNumberPicker(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         binding = CustomNumberPickerBinding.inflate(LayoutInflater.from(context), this, true);
 
-        numberPicker = new NumberPicker(context);
-
         try (TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.CustomNumberPicker)) {
             binding.label.setText(array.getString(R.styleable.CustomNumberPicker_android_label));
-            dialogTitle = array.getString(R.styleable.CustomNumberPicker_title);
-            numberPicker.setMinValue(array.getInt(R.styleable.CustomNumberPicker_android_valueFrom, 0));
-            numberPicker.setMaxValue(array.getInt(R.styleable.CustomNumberPicker_android_valueTo, 0));
+            binding.numberPicker.setMinValue(array.getInt(R.styleable.CustomNumberPicker_android_valueFrom, 0));
+            binding.numberPicker.setMaxValue(array.getInt(R.styleable.CustomNumberPicker_android_valueTo, 0));
         }
 
-        AlertDialog dialog = new AlertDialog.Builder(context)
-                .setTitle(dialogTitle)
-                .setView(numberPicker)
-                .setPositiveButton("OK", (dialogInterface, i) -> {
-                    binding.numberInput.setText(String.valueOf(numberPicker.getValue()));
-                })
-                .setOnDismissListener(dialogInterface -> {
-                    Integer oldNumber = getSelectedNumber();
-                    numberPicker.setValue(oldNumber == null ? numberPicker.getMinValue() : oldNumber);
-                })
-                .create();
-
-        binding.numberInput.setFocusable(false);
-        binding.numberInput.setOnClickListener(view -> {
-            dialog.show();
-        });
     }
 
     public Integer getSelectedNumber() {
-        String number = binding.numberInput.getText().toString();
-
-        return number.isEmpty()
-                ? null
-                : Integer.valueOf(number);
+        return binding.numberPicker.getValue();
     }
 
     public void setNumberChangedListener(Consumer<Integer> listener) {
-        binding.numberInput.addTextChangedListener((TextChangedListener) editable ->
-                listener.accept(getSelectedNumber()));
+        binding.numberPicker.setOnValueChangedListener((picker, number, i1) -> {
+            listener.accept(number);
+        });
     }
 
 }
